fb08a3cc902980f3d0703b70d326d376f22ae3b3
wojtek404 <wojtek404@o2.pl>
2017-12-23 22:56:27 +0100

    Przechowywanie zahaszowanych haseł oraz logowanie restowe ze zwracaniem tokena.

--- server/createDB.py
+++ server/createDB.py
@@ -2,14 +2,38 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
+    username  = Column(String(32), nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+        
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)       
+        
+    def generate_auth_token(self, secret_key, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token, secret_key):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
 	
 class Group(Base):
 	__tablename__ = 'group'
---
+++ server/main.py
@@ -0,0 +1,84 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from createDB import Base, User, Group, GroupInvitation, GroupUsers
+from alchemyEncoder import new_alchemy_encoder
+
+from flask import Flask, jsonify, abort, request, g
+from flask_httpauth import HTTPBasicAuth
+import json
+from passlib.apps import custom_app_context as pwd_context
+
+#flask app
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+#database SQLAlchemy
+engine = create_engine('sqlite:///order_food.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+#auth
+auth = HTTPBasicAuth()
+    
+@app.route('/register', methods = ['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if session.query(User).filter(User.username==username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'id': user.id, 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(app.secret_key, username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = session.query(User).filter(User.username==username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+     
+@app.route('/login')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(app.secret_key, 600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+    
+@app.route('/users/<int:id>')
+@auth.login_required
+def get_user(id):
+    user = session.query(User).filter(User.id==id)
+    if not user:
+        abort(400)
+    return jsonify({ 'id': user.id, 'username': user.username })
+    
+@app.route('/users', methods=['GET'])
+@auth.login_required
+def get_users():
+    list = [{ 'id': user.id, 'username': user.username } for x in session.query(User).all()]
+    return jsonify({'users': list})
+
+@app.route('/groupUsers/<int:id>', methods=['GET'])
+@auth.login_required
+def get_group_users(group_id):
+    groupUsers = [json.dumps(gu.user, cls=new_alchemy_encoder(), check_circular=False) for gu in session.query(GroupUsers).filter(GroupUsers.group_id==group_id).all()]
+    return jsonify({'users': groupUsers})
+
+@app.route('/groups', methods=['GET'])
+@auth.login_required
+def get_groups():
+    groups = [json.dumps(x, cls=new_alchemy_encoder(), check_circular=False) for x in session.query(Group).all()]
+    return jsonify({'groups': groups})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
